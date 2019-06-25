33e50cb04bfbcfacc553fcf5ff02be1130d0ee91
Jorge Sánchez <jorge.sanchez@playgiga.com>
2019-06-25 15:45:46 +0200

    When users generate a catalog version they must can add description field.

--- public/js/main.js
+++ public/js/main.js
@@ -6,20 +6,37 @@ $( document ).ready(function() {
 
     $( ".catalog-versions .list .list-body" ).selectable();
 
-    $('.catalog-versions .add .btn').click(function() {
-        let data = {
-            'catalogId': 28,
-            'description': 'I love you 3000.'
-        };
+    $('.catalog-versions .add .btn').click(function(event) {
+        $('.modal.generate-catalog-modal').modal('open');
+        event.preventDefault();
+    });
 
+    $('.generate-catalog-form .btn').click(function(event) {
+
+        let data = {
+            'catalogId': $('.catalog-filter select option:selected').val(),
+            'description': $('.input-description').val()
+        };
+        console.log(data);
         let progressHtml = '<div class="progress">'
             + '<div class="indeterminate"></div>'
             +'</div>';
-        $('.modal .modal-content').html(progressHtml);
-        $('.modal').modal('open');
 
-        $.post( "http://localhost:20443/api/v1/catalogs-versions", data,  function(response) {
+        $('.modal.default .modal-content').html(progressHtml);
+        $('.modal.generate-catalog-modal').modal('close');
+        $('.modal.default').modal('open');
+
+        $.post( "http://localhost:20443/api/v1/catalogs-versions", data, function(response) {
+            event.preventDefault();
             console.log(response);
+
+            // Check if is the first catalog
+            if ($('.catalog-versions .list').length == 0) {
+                location.reload();
+
+                return false;
+            }
+
             $('.catalog-versions .list .list-body').prepend(
                 '<tr class="catalog-version" data-catalog-version-id="' + response.catalogVersionId + '">' +
                 '  <td>' + response.catalogVersionId + '</td>' +
@@ -31,16 +48,17 @@ $( document ).ready(function() {
             );
 
             $( ".catalog-versions .list .list-body" ).selectable();
-
             $('.modal .modal-content')
                 .html('<article class="card-panel green lighten-4">Catalog version has been created successfully.</article>');
-        }, "json")
-        .fail(function(response) {
+
+        }, "json").fail(function(response) {
             console.log(response);
             // Show alert
             $('.modal .modal-content')
                 .html('<article class="card-panel red lighten-4">' + response.responseJSON.error + '</article>');
         });
+
+
     });
 
     // Select catalog
